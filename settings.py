9b957c0eba7ee461b166dcc4929ec83aae2ad196
vexubi <nacluunxam@tutamail.com>
2025-01-29 20:58:07 +0000

    Get keys from awg container

--- settings.py
+++ settings.py
@@ -1,9 +1,26 @@
 import json
+from os import wait
 import subprocess
 
 
 def load_wg_server_private_key(container_id: str) -> str:
-    tokens = ['docker', 'exec', container_id, 'cat', '/opt/amnezia/awg/wireguard_server_private_key.key']
+    command_list = ['cat', '/opt/amnezia/awg/wireguard_server_private_key.key']
+    return _execute_arbitrary_command_in_container(container_id, command_list)
+
+
+def load_wg_server_public_key(container_id: str) -> str:
+    command_list = ['cat', '/opt/amnezia/awg/wireguard_server_public_key.key']
+    return _execute_arbitrary_command_in_container(container_id, command_list)
+
+
+def load_wg_server_preshared_key(container_id: str) -> str:
+    command_list = ['cat', '/opt/amnezia/awg/wireguard_psk.key']
+    return _execute_arbitrary_command_in_container(container_id, command_list)
+
+
+def _execute_arbitrary_command_in_container(container_id: str, command_list: list[str]) -> str:
+    tokens = ['docker', 'exec', container_id, *command_list]
+
     try:
         result = subprocess.run(tokens, check=True,
                                 text=True, stdout=subprocess.PIPE,
@@ -22,6 +39,6 @@ with open('settings.json') as f:
 
 DOCKER_CONTAINER_ID = settings.get("amnezia-awg").get("docker-container-id")
 
-WG_SERVER_PRIVATE_KEY = load_wg_server_private_key(DOCKER_CONTAINER_ID)
-
-print(WG_SERVER_PRIVATE_KEY)
+WG_SERVER_PRIVATE_KEY = load_wg_server_private_key(DOCKER_CONTAINER_ID).strip('\n')
+WG_SERVER_PUBLIC_KEY = load_wg_server_public_key(DOCKER_CONTAINER_ID).strip('\n')
+WG_SERVER_PSK = load_wg_server_preshared_key(DOCKER_CONTAINER_ID).strip('\n')

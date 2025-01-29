fdd5cc65e6cefa96f97de6515fda833c3c0a8b48
weqnek <nacluunxam@tutamail.com>
2025-01-29 22:37:51 +0300

    Get wg server private key

--- settings.py
+++ settings.py
@@ -4,11 +4,14 @@ import subprocess
 
 def load_wg_server_private_key(container_id: str) -> str:
     tokens = ['docker', 'exec', container_id, 'cat', '/opt/amnezia/awg/wireguard_server_private_key.key']
-    try: 
-        result = subprocess.run(tokens, check=True, text=True)
+    try:
+        result = subprocess.run(tokens, check=True,
+                                text=True, stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
         return result.stdout
 
     except subprocess.CalledProcessError as e:
+        # TODO
         print(e)
         exit()
 
@@ -17,8 +20,8 @@ with open('settings.json') as f:
     settings = json.load(f)
 
 
-DOCKER_CONTAINER_ID = settings.get("docker-container-id")
+DOCKER_CONTAINER_ID = settings.get("amnezia-awg").get("docker-container-id")
 
 WG_SERVER_PRIVATE_KEY = load_wg_server_private_key(DOCKER_CONTAINER_ID)
 
-print(WG_SERVER_PRIVATE_KEY) 
+print(WG_SERVER_PRIVATE_KEY)

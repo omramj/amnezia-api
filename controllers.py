2a4fb4096b77cf7a17b69d44064c7855aa110bc7
vexubi <nacluunxam@tutamail.com>
2025-02-02 04:30:50 +0000

    Improve formatting

--- controllers.py
+++ controllers.py
@@ -1,5 +1,4 @@
 import subprocess
-import json
 
 import docker
 from docker.models.containers import Container, ExecResult
@@ -10,7 +9,6 @@ class Executor:
         pass
 
     def _execute_shell_command(self, command: str) -> str:
-
         try:
             result = subprocess.run(command, check=True,
                                     text=True, shell=True, stdout=subprocess.PIPE,
@@ -24,20 +22,17 @@ class Executor:
 
 
     def _write_to_file(self, filepath: str, data: str) -> None:
-
         with open(f"{filepath}", 'w') as file:
             file.write(data)
 
 
     def _remove_file(self, filepath: str) -> None:
-
         subprocess.run(f"rm {filepath}", shell=True)
 
 
 class ServerController(Executor):
     def __init__(self):
-        self.xray: XrayContainerController
-
+        self.xray_controller: XrayContainerController
         self.docker_client = docker.from_env()
         self.initialize_container_controllers()
 
@@ -47,11 +42,10 @@ class ServerController(Executor):
 
 
     def initialize_container_controllers(self):
-
         for container in self.get_installed_containers():
             match container.name:
                 case "amnezia-xray":
-                    self.xray = XrayContainerController(container)
+                    self.xray_controller = XrayContainerController(container)
                 case _:
                     #TODO
                     pass
@@ -65,39 +59,32 @@ class ContainerController(Executor):
     def restart_container(self) -> None:
         self.container.restart()
 
+
     def execute_arbitrary_command_in_container(self, command: str) -> ExecResult:
-
         result = self.container.exec_run(f"{command}")
-
         return result
 
 
     def copy_file_into_container(self, from_path: str, to_path: str) -> str:
         # copy a file from host into container
-
         full_command = f"docker cp {from_path} {self.container.id}://{to_path}"
-        
         print(full_command)
         return self._execute_shell_command(full_command)
 
 
 class XrayContainerController(ContainerController):
-# https://github.com/amnezia-vpn/amnezia-client/blob/dev/client/configurators/xray_configurator.cpp
-
     def __init__(self, container: Container) -> None:
         self.container = container
 
 
-    def get_server_config(self) -> dict:
+    def get_server_config(self) -> str:
         command_list = "cat /opt/amnezia/xray/server.json"
         result = self.execute_arbitrary_command_in_container(command_list)
-        return json.loads(result.output)
+        return result[1].decode()
 
 
-    def update_server_config(self, new_config: dict) -> str:
-        
-        new_config_json = json.dumps(new_config, indent=4)
-        command = f""" sh -c "echo '{new_config_json}' > /opt/amnezia/xray/server1.json" """
+    def update_server_config(self, new_config: str) -> str:
+        # command = f""" sh -c "echo '{new_config}' > /opt/amnezia/xray/server.json" """
 
         # First: save the config to a host.
         # Second: copy from host to the container. 
@@ -107,9 +94,8 @@ class XrayContainerController(ContainerController):
         # TODO in the future.
 
         filepath = "tmp/server.json"
-        self._write_to_file(filepath=filepath, data=new_config_json)
+        self._write_to_file(filepath=filepath, data=new_config)
         result = self.copy_file_into_container(filepath,
-                                               "/opt/amnezia/xray/server1.json")
+                                               "/opt/amnezia/xray/server.json")
         self._remove_file(filepath)
-
         return result

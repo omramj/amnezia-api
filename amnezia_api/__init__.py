3074493e35f6ec2628291f43efded655a556ab21
weqnek <nacluunxam@tutamail.com>
2025-02-08 14:09:53 +0300

    Add status message for convenience

--- amnezia_api/__init__.py
+++ amnezia_api/__init__.py
@@ -9,11 +9,16 @@ def create_app() -> Flask:
     server = ServerController()
 
     
-    @app.route(f"/{settings.secret_url_string}/xray/create-config", methods=["POST"])
+    @app.route(f"/{settings.secret_url_string}/xray/create-config", methods=["GET", "POST"])
     def create_xray_config():
         if request.method == "POST":
             return server.xray.create_user_config()
         else:
             return "Hello there"
+
+
+    @app.route(f"/{settings.secret_url_string}/status", methods=["GET"])
+    def show_status_message():
+        return "This message indicates that amnezia-api backend is accessible"
 
     return app
